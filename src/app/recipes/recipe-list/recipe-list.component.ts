14c30a0016d97e66f91ba2792922a28e61a9f926
Sushant Gambhir <gambhirboy1234@gmail.com>
2024-05-23 22:07:43 +0530

    sfwef

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { RecipeItemComponent } from './recipe-item/recipe-item.component';
+import { Recipe } from '../recipe.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-recipe-list',
   standalone: true,
-  imports: [RecipeItemComponent],
+  imports: [RecipeItemComponent, CommonModule],
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent implements OnInit 
 {
-    recipes = [];
+    recipes: Recipe[] = [
+      new Recipe("Dummy Recipe","Just a Test", 
+      "https://www.foodandwine.com/thmb/MsTd5zgsuEHBo1w-vWuuYQno0mw=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/FAW-recipes-pasta-sausage-basil-and-mustard-hero-06-cfd1c0a2989e474ea7e574a38182bbee.jpg"),
+      new Recipe("Dummy Recipe","Just a Test", 
+      "https://www.foodandwine.com/thmb/MsTd5zgsuEHBo1w-vWuuYQno0mw=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/FAW-recipes-pasta-sausage-basil-and-mustard-hero-06-cfd1c0a2989e474ea7e574a38182bbee.jpg")
+    ];
 
     constructor() {}
     ngOnInit() {}
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
+import { Ingredient } from '../shared/ingredient';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-shopping-list',
   standalone: true,
-  imports: [ShoppingEditComponent],
+  imports: [ShoppingEditComponent, CommonModule],
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.css'
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit{
 
+  ingredients: Ingredient[] = [
+    new Ingredient("Apple",5),
+    new Ingredient("Tomato",10)
+  ];
+
+  ngOnInit(): void {
+      
+  }
 }
